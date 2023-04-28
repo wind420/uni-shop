0e6876cf252b426836a81f357fd0e1f7bb4316b7
云浅月 <1527977138@qq.com>
2023-04-28 14:02:55 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 导入网略请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 配置请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 请求开始之前做一些事情(请求拦截器)
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中···'
+	})
+}
+
+// 请求完成之后做一些事情(响应拦截器)
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装弹窗的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
